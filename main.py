78555dd192a6d51013c7bdb34fba713d560e4922
Eduardo Lovera Hirano <eduardoloverahirano@gmail.com>
2023-01-10 09:34:34 -0300

    refactor: made the mongoDB connection a class object.

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ from pymongo import MongoClient
 
 load_dotenv(find_dotenv())
 
-password = os.environ.get('MONGODB_PWD')
-connection_string = f'mongodb+srv://ehirano:{password}@dataproject.5zkbana.mongodb.net/?retryWrites=true&w=majority'
-client = MongoClient(connection_string)
+class MongoDB:
+    def __init__(self) -> None:
+        MONGODB_PWD = os.environ.get('MONGODB_PWD')
+        connection_string = f'mongodb+srv://elhirano:{MONGODB_PWD}@dataproject.euatmcr.mongodb.net/?retryWrites=true&w=majority'
+        self.client = MongoClient(connection_string)
+
+    def list_dbs(self):
+        return self.client.list_database_names()
+            
+    def list_collections(self):
+        return self.client.data_project.list_collection_names()
+
+mdb = MongoDB()
+print(mdb.list_dbs())
+print(mdb.list_collections())
